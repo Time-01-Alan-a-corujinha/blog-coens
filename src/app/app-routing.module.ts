fdcd3b73ba78398df31fbde3b3f11cbbbc5203a2
Eduardo Viega <eduardoviega12@gmail.com>
2023-10-01 21:57:51 -0300

    SCRUM-46 - Implementação do header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { MenuComponent } from './components/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'menu', pathMatch: 'full'},
+  {path: 'menu', component: MenuComponent, children: [
+    {path: '', component: InicioComponent},
+    {path: 'noticias', component: NoticiasComponent}
+  ]},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NoticiasComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatIconModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
