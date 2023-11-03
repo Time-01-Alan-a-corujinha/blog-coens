631cd1bf2e69e1db2d10dda1a9286cd0371d7d65
Eduardo Viega <eduardoviega12@gmail.com>
2023-11-02 22:22:27 -0300

    SCRUM-84 Tela de inicio faltando posts

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -7,7 +7,11 @@ import { InicioService } from './inicio.service';
   styleUrls: ['./inicio.component.scss']
 })
 export class InicioComponent implements OnInit {
-  public dados: any
+  public dados: any = {
+    descricao: <string> "",
+    videoInstitucionalLink: <string> "",
+    barraLateral: <object> []
+  }
 
   constructor(private inicioService: InicioService) { }
 
@@ -18,7 +22,19 @@ export class InicioComponent implements OnInit {
   buscarDados() {
     this.inicioService.getInicio().subscribe({
       next: (dados: any) => {
-        this.dados = dados.data.attributes
+        const infos = dados.data.attributes
+        console.log(infos);
+
+        this.dados.descricao = infos.descricao
+        this.dados.videoInstitucionalLink = infos.videoInstitucional.data.attributes.url
+
+        this.dados.barraLateral.push({titulo: "Tipo de Curso", descricao: infos.tipoCurso})
+        this.dados.barraLateral.push({titulo: "Modalidade", descricao: infos.modalidade})
+        this.dados.barraLateral.push({titulo: "Duração", descricao: infos.duracao})
+        this.dados.barraLateral.push({titulo: "Turno", descricao: infos.Turno})
+        this.dados.barraLateral.push({titulo: "Número de Vagas", descricao: infos.numeroDeVagas})
+        this.dados.barraLateral.push({titulo: "Forma de Ingresso", descricao: infos.formaDeIngresso})
+        
         console.log(this.dados);
       }, error: (err) => {
         console.log(err)
