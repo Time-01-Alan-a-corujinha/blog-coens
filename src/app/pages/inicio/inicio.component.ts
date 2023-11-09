fc11eb5813551262a961f1b2649f6145b8d94cec
CervinoB <jcervinobarbosa@gmail.com>
2023-11-09 20:08:52 -0300

    feat: add markdown converter

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InicioService } from './inicio.service';
+const showdown = require('showdown');
+// import {showdown} from 'showdown';
 
 @Component({
   selector: 'app-inicio',
@@ -23,20 +25,32 @@ export class InicioComponent implements OnInit {
     this.inicioService.getInicio().subscribe({
       next: (dados: any) => {
         const infos = dados.data.attributes
-
-        this.dados.descricao = infos.descricao
+        
+        this.preencheDescricao(infos.descricao);
+        
         this.dados.videoInstitucionalLink = infos.videoInstitucional.data.attributes.url
-
+        
         this.dados.barraLateral.push({titulo: "Tipo de Curso", descricao: infos.tipoCurso})
         this.dados.barraLateral.push({titulo: "Modalidade", descricao: infos.modalidade})
         this.dados.barraLateral.push({titulo: "Duração", descricao: infos.duracao})
         this.dados.barraLateral.push({titulo: "Turno", descricao: infos.Turno})
         this.dados.barraLateral.push({titulo: "Número de Vagas", descricao: infos.numeroDeVagas})
         this.dados.barraLateral.push({titulo: "Forma de Ingresso", descricao: infos.formaDeIngresso})
+        
       }, error: (err) => {
         console.log(err)
       }
     });
+  }
+
+  preencheDescricao(desc: string) {
+    let texto = document.getElementById('texto');
+    if (texto) {
+      let converter = new showdown.Converter();
+      let descricaoConvertida = converter.makeHtml(desc);
+      
+      texto.innerHTML = descricaoConvertida
+    } 
   }
 
 }
