39f816c3992e362c09cd99f83717e64684b757a5
EduardoRizzo22 <eduardorizzo@alunos.utfpr.edu.br>
2023-11-19 15:45:28 -0300

    SCRUM-139

--- src/app/pages/coens/docentes/docentes.component.ts
+++ src/app/pages/coens/docentes/docentes.component.ts
@@ -7,7 +7,9 @@ import { DocentesService } from './docentes.service';
   styleUrls: ['./docentes.component.scss']
 })
 export class DocentesComponent implements OnInit {
-
+  public dados: any = {
+    docentes: <object>[]
+  }
   constructor(private docentesService: DocentesService) { }
 
   ngOnInit(): void {
@@ -18,6 +20,17 @@ export class DocentesComponent implements OnInit {
     this.docentesService.getDocentes().subscribe({
       next: (dados:any) => { 
         console.log(dados)
+
+        const info = dados.data.attributes
+
+        this.dados.docentes.push({titulo: "Nome", descricao: info.nome})
+        this.dados.docentes.push({titulo: "Email", descricao: info.email})
+        this.dados.docentes.push({titulo: "lattesLink", descricao: info.lattesLink})
+        this.dados.docentes.push({titulo: "Nivel Superior", descricao: info.nivelSuperior})
+        this.dados.docentes.push({titulo: "ProfileImagem", descricao: info.ProfileImagem.dados.attributes.url})
+
+      },error:(err) =>{
+        console.log(err)
       }
     })
   }
