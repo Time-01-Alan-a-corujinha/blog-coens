4dd02df4d3cb1993cf1fdfc05ab2770d39425654
Eduardo Viega <eduardoviega12@gmail.com>
2023-11-23 15:46:18 -0300

    SCRUM-132 - Loading funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { BoxtextComponent } from './components/boxtext/boxtext.component';
 import { MeusPostsComponent } from './pages/noticias/meus-posts/meus-posts.component';
 import { RecentesComponent } from './pages/noticias/recentes/recentes.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { SafePipePipe } from './components/safe-pipe/safe-pipe.pipe';
 import { PosGraduacaoComponent } from './pages/pos-graduacao/pos-graduacao.component';
 import { ConhecaOAlanComponent } from './pages/conheca-o-alan/conheca-o-alan.component';
@@ -34,6 +34,9 @@ import { TccComponent } from './pages/curso/tcc/tcc.component';
 import { CentroAcademicoComponent } from './pages/curso/centro-academico/centro-academico.component';
 import { NucleoDocenteEstruturanteComponent } from './pages/coens/nucleo-docente-estruturante/nucleo-docente-estruturante.component';
 import { ColegiadoComponent } from './pages/coens/colegiado/colegiado.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { LoadingInterceptor } from './components/loading/loading.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -63,6 +66,7 @@ import { ColegiadoComponent } from './pages/coens/colegiado/colegiado.component'
     CentroAcademicoComponent,
     NucleoDocenteEstruturanteComponent,
     ColegiadoComponent,
+    LoadingComponent,
   ],
   imports: [
     BrowserModule,
@@ -74,7 +78,13 @@ import { ColegiadoComponent } from './pages/coens/colegiado/colegiado.component'
     MatExpansionModule,
     MatTableModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
